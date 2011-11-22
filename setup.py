5a4bcd4a84ac104d3926f26fe6beb1ebb374732e
Erik Rose <erik@mozilla.com>
2011-11-22 14:51:18 -0800

    We need Python 3.2.3 or better, to fix a curses bug.

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: User Interfaces',
