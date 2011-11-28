37ee22a14ac643223eca31fe9da19e67820458bf
Erik Rose <erik@mozilla.com>
2011-11-28 13:32:17 -0800

    Replace the remaining occurrences of "django-tidings" in the Sphinx makefiles.

--- docs/make.bat
+++ docs/make.bat
@@ -95,9 +95,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\django-tidings.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\blessings.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\django-tidings.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\blessings.ghc
 	goto end
 )
 
