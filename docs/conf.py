b5031a0621f45aba2c61d51ddb7eba59af3b93bd
Erik Rose <grinch@grinchcentral.com>
2012-06-02 18:23:19 -0700

    Bump version to 1.4 in docs config.

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ copyright = u'2011, Erik Rose'
 # built documents.
 #
 # The short X.Y version.
-version = '1.3'
+version = '1.4'
 # The full version, including alpha/beta/rc tags.
 release = version
 
