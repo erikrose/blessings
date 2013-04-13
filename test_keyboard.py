277cb50c9ad2e2600374b356839999cd1e3be8c8
jquast <contact@jeffquast.com>
2013-04-12 23:27:21 -0400

    add nother *human* test for animations
    this is really just a test of timing performance, revived an old alogorithm i made as a kid and wrapped it into a simple game, called "newton's nightmare".

--- test_keyboard.py
+++ test_keyboard.py
@@ -1,13 +1,148 @@
 #!/usr/bin/env python
 import blessings
+import random
 import sys
-"""Human tests; made interesting with a 'video game'
-"""
+"""Human tests; made interesting with various 'video games' """
 
 def main():
     play_whack_a_key()
+    play_newtons_nightmare()
+
+def play_newtons_nightmare():
+    """
+    Demonstration of animation and 'movement'.
+    Go ahead, make yourself a rouge-like :-)
+    """
+    term = blessings.Terminal()
+    n_balls = int((term.width * term.height) * .01)
+    balls = list()
+    for n in range(n_balls):
+        ball = {
+            'x': float(random.randint(1, term.width)),
+            'y': float(random.randint(1, term.height)),
+            'x_velocity': float(random.randint(1, 10)) * .1,
+            'y_velocity': float(random.randint(1, 10)) * .1,
+            'color': random.randint(1, 7)
+            }
+        ball['x_pos'] = ball['y_pos'] = -1
+        balls.append(ball)
+    gravity_x = term.width / 2
+    gravity_y = term.height / 2
+    gravity_xs = 0.0
+    gravity_ys = 0.0
+
+    def cycle(ball):
+        if ball['x'] > gravity_x:
+            ball['x_velocity'] -= 0.01
+        else:
+            ball['x_velocity'] += 0.01
+        if ball['y'] > gravity_y:
+            ball['y_velocity'] -= 0.01
+        else:
+            ball['y_velocity'] += 0.01
+        ball['x_velocity'] = max(ball['x_velocity'], -1.0)
+        ball['x_velocity'] = min(ball['x_velocity'], 1.0)
+        ball['y_velocity'] = max(ball['y_velocity'], -1.0)
+        ball['y_velocity'] = min(ball['y_velocity'], 1.0)
+        return ball
+
+    def chk_die(balls):
+        for ball in balls:
+            if (ball['x_pos'] == int(gravity_x)
+                    and ball['y_pos'] == int(gravity_y)):
+                return True
+
+    def step(balls):
+        for ball in balls:
+            ball = cycle(ball)
+            xv = ball['x_velocity']
+            yv = ball['y_velocity']
+            ball['x'] += xv
+            ball['y'] += yv
+        return balls
+
+    def refresh(term, balls):
+        # erase last gravity glpyh
+        sys.stdout.write('\b ')
+        def outofrange(ball):
+            return (ball['y_pos'] < 0
+                    or ball['y_pos'] > (term.height - 1)
+                    or ball['x_pos'] < 0
+                    or ball['x_pos'] > (term.width - 1))
+        def erase(ball):
+            sys.stdout.write(term.move(ball['y_pos'], ball['x_pos']))
+            sys.stdout.write(term.color(ball['color']))
+            sys.stdout.write('.')
+            sys.stdout.write(term.normal)
+        def draw(ball):
+            sys.stdout.write(term.bold)
+            sys.stdout.write(term.color(ball['color']))
+            sys.stdout.write(term.move(ball['y_pos'], ball['x_pos']) + u'*')
+            sys.stdout.write(term.normal)
+        for ball in balls:
+            sx = int(ball['x'])
+            sy = int(ball['y'])
+            if ball['x_pos'] != sx or ball['y_pos'] != sy:
+                # erase old ball
+                if not outofrange(ball):
+                    erase(ball)
+                # update position and draw
+                ball['x_pos'] = sx
+                ball['y_pos'] = sy
+                if not outofrange(ball):
+                    draw(ball)
+        sys.stdout.write(term.move(int(gravity_y), int(gravity_x)))
+        sys.stdout.write('+')
+        sys.stdout.flush()
+
+    delay = 0.05
+    score = 0
+    with term.hidden_cursor() and term.cbreak():
+        sys.stdout.write(term.clear + term.home)
+        while True:
+            score += 1
+            delay = max(delay - 0.00001, 0.01)
+            balls = step(balls)
+            gravity_x += gravity_xs
+            gravity_y += gravity_ys
+            if gravity_x >= (term.width - 1) or gravity_x <= 1:
+                gravity_xs *= -1
+            if gravity_y >= (term.height - 1) or gravity_y <= 1:
+                gravity_ys *= -1
+            refresh(term, balls)
+            if chk_die(balls):
+                break
+            inp = term.inkey(delay)
+            if inp is None:
+                continue
+            if inp in (u'q', 'Q'):
+                break
+            if (inp.code == term.KEY_UP
+                    and gravity_y > 1.0
+                    and gravity_ys > -1.0):
+                gravity_ys -= 0.2
+            elif (inp.code == term.KEY_DOWN
+                    and gravity_y < (term.height - 1)
+                    and gravity_ys < 1.0):
+                gravity_ys += 0.2
+            elif (inp.code == term.KEY_LEFT
+                    and gravity_x > 1.0
+                    and gravity_xs > -1.0):
+                gravity_xs -= 0.3
+            elif (inp.code == term.KEY_RIGHT
+                    and gravity_x < (term.width - 1)
+                    and gravity_xs < 1.0):
+                gravity_xs += 0.3
+        print term.move(term.height, 0)
+        print 'Your final score was', score
+        print 'press any key'
+        term.inkey()
 
 def play_whack_a_key():
+    """
+    Displays all known key capabilities that may match the terminal.
+    As each key is pressed on input, it is lit up and points are scored.
+    """
     term = blessings.Terminal()
     score = 0
     level = 0
@@ -94,12 +229,14 @@ def play_whack_a_key():
                 hit_unicode += 1
                 if hit_unicode < 5:
                     score, level = add_score (score, 100, level)
-    print term.move(term.height, 0)
-    print 'Your final score was', score, 'at level', level
-    if hit_highbit > 0:
-        print 'You hit', hit_highbit, 'extended ascii characters'
-    if hit_unicode > 0:
-        print 'You hit', hit_unicode, 'unicode characters'
+        print term.move(term.height, 0)
+        print 'Your final score was', score, 'at level', level
+        if hit_highbit > 0:
+            print 'You hit', hit_highbit, 'extended ascii characters'
+        if hit_unicode > 0:
+            print 'You hit', hit_unicode, 'unicode characters'
+        print 'press any key'
+        term.inkey()
 
 if __name__ == '__main__':
     main()
