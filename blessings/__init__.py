c930f6ab373345f8159a48e494cf6bf8df95fe43
Michel Albert <michel@albert.lu>
2013-08-09 18:17:27 +0200

    Added an "approximate_rgb" function.
    Also covered 256 colour codes with test. Not 100% sure if those test are
    any good though.

--- blessings/__init__.py
+++ blessings/__init__.py
@@ -410,6 +410,29 @@ class Terminal(object):
         notion of "normal"."""
         return FormattingString(formatting, self.normal)
 
+    def approximate_rgb(self, r, g, b):
+        if self.number_of_colors != 256:
+            return self.color(7)
+
+        if r == g == b:
+            offset = int(r / (256.0 / 24.0))
+            index = 232 + offset
+        else:
+            roundint = lambda n, p: (n + p / 2) / p * p
+
+        # Technically this ought to clamp to 0x5f, 0x87, 0xaf, and 0xd7
+        # rather than 0x33, 0x66, 0x99, and 0xcc, but who the hell came
+        # up with that? Seriously.
+        clamped_r = roundint(r, 0x33)
+        clamped_g = roundint(g, 0x33)
+        clamped_b = roundint(b, 0x33)
+
+        col = clamped_b / 0x33
+        row = clamped_g / 0x33
+        face = clamped_r / 0x33
+        index = 36 * face + 6 * row + col + 16
+
+        return self.color(index)
 
 def derivative_colors(colors):
     """Return the names of valid color variants, given the base colors."""
