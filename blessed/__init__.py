94de2acf00ef42f17049a597772282ecc1f844b3
jquast <contact@jeffquast.com>
2014-03-23 02:20:44 -0700

    blessings -> blessed rename

--- blessed/__init__.py
+++ blessed/__init__.py
@@ -5,7 +5,7 @@ import platform as _platform
 if ('3', '0', '0') <= _platform.python_version_tuple() < ('3', '2', '2+'):
     # Good till 3.2.10
     # Python 3.x < 3.2.3 has a bug in which tparm() erroneously takes a string.
-    raise ImportError('Blessings needs Python 3.2.3 or greater for Python 3 '
+    raise ImportError('Blessed needs Python 3.2.3 or greater for Python 3 '
                       'support due to http://bugs.python.org/issue10570.')
 
 
