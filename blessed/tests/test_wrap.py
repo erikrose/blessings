42fedd4cac5b8f7d0a7ce42b60d312032d965925
jquast <contact@jeffquast.com>
2014-03-23 02:21:38 -0700

    test various textwrap options

--- blessed/tests/test_wrap.py
+++ blessed/tests/test_wrap.py
@@ -117,3 +117,58 @@ def test_SequenceWrapper_27(all_terms):
         assert (len(internal_wrapped[-1]) == t.length(my_wrapped_colored[-1]))
 
     child(kind=all_terms)
+
+
+def test_SequenceWrapper_drop_whitespace_subsequent_indent():
+    """Test that text wrapping matches internal extra options."""
+    WIDTH = 10
+
+    @as_subprocess
+    def child():
+        # build a test paragraph, along with a very colorful version
+        t = TestTerminal()
+        pgraph = u''.join(
+            ('a', 'ab', 'abc', 'abcd', 'abcde', 'abcdef', 'abcdefgh',
+             'abcdefghi', 'abcdefghij', 'abcdefghijk', 'abcdefghijkl',
+             'abcdefghijklm', 'abcdefghijklmn', 'abcdefghijklmno',) * 4)
+
+        pgraph_colored = u''.join([
+            t.color(n % 7) + t.bold + ch
+            for n, ch in enumerate(pgraph)])
+
+        internal_wrapped = textwrap.wrap(pgraph, width=WIDTH,
+                                         break_long_words=False,
+                                         drop_whitespace=True,
+                                         subsequent_indent=4)
+        my_wrapped = t.wrap(pgraph, width=WIDTH)
+        my_wrapped_colored = t.wrap(pgraph_colored, width=WIDTH,
+                                    drop_whitespace=True,
+                                    subsequent_indent=4)
+
+        # ensure we textwrap ascii the same as python
+        assert (internal_wrapped == my_wrapped)
+
+        # ensure our first and last line wraps at its ends
+        first_l = internal_wrapped[0]
+        last_l = internal_wrapped[-1]
+        my_first_l = my_wrapped_colored[0]
+        my_last_l = my_wrapped_colored[-1]
+        assert (len(first_l) == t.length(my_first_l))
+        assert (len(last_l) == t.length(my_last_l))
+        assert (len(internal_wrapped[-1]) == t.length(my_wrapped_colored[-1]))
+
+        # ensure our colored textwrap is the same line length
+        assert (len(internal_wrapped) == len(my_wrapped_colored))
+        # test subsequent_indent=
+        internal_wrapped = textwrap.wrap(pgraph, WIDTH, break_long_words=False,
+                                         subsequent_indent=' '*4)
+        my_wrapped = t.wrap(pgraph, width=WIDTH,
+                            subsequent_indent=' '*4)
+        my_wrapped_colored = t.wrap(pgraph_colored, width=WIDTH,
+                                    subsequent_indent=' '*4)
+
+        assert (internal_wrapped == my_wrapped)
+        assert (len(internal_wrapped) == len(my_wrapped_colored))
+        assert (len(internal_wrapped[-1]) == t.length(my_wrapped_colored[-1]))
+
+    child()
