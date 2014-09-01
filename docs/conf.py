edaa38bb8f0b1228f1186d15ae268155ce33df2c
jquast <contact@jeffquast.com>
2014-09-01 16:00:08 -0700

    release version 1.9.3

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ copyright = u'2014 Jeff Quast, 2011 Erik Rose'
 # built documents.
 #
 # The short X.Y version.
-version = '1.9.2'
+version = '1.9.3'
 
 # The full version, including alpha/beta/rc tags.
 release = version
