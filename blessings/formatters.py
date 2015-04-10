1c07684b0f26ac7aaedf6f175bbb7459051faefe
Jeff Quast <jquast@io.com>
2015-04-09 20:21:13 -0700

    pep257 and static analysis for formatters.py

--- blessings/formatters.py
+++ blessings/formatters.py
@@ -1,43 +1,63 @@
-"This sub-module provides formatting functions."
+"""This sub-module provides formatting functions."""
 # standard imports
 import curses
-import sys
 
 # 3rd-party
 import six
 
-_derivatives = ('on', 'bright', 'on_bright',)
 
-_colors = set('black red green yellow blue magenta cyan white'.split())
-_compoundables = set('bold underline reverse blink dim italic shadow '
-                     'standout subscript superscript'.split())
+def _make_colors():
+    """
+    Return set of valid colors and their derivatives.
+
+    :rtype: set
+    """
+    derivatives = ('on', 'bright', 'on_bright',)
+    colors = set('black red green yellow blue magenta cyan white'.split())
+    return set(['_'.join((_derivitive, _color))
+                for _derivitive in derivatives
+                for _color in colors]) | colors
+
+
+def _make_compoundables(colors):
+    """
+    Return given set ``colors`` along with all "compoundable" attributes.
+
+    :param set colors: set of color names as string.
+    :rtype: set
+    """
+    _compoundables = set('bold underline reverse blink dim italic shadow '
+                         'standout subscript superscript'.split())
+    return colors | _compoundables
+
 
 #: Valid colors and their background (on), bright, and bright-bg derivatives.
-COLORS = set(['_'.join((derivitive, color))
-              for derivitive in _derivatives
-              for color in _colors]) | _colors
+COLORS = _make_colors()
 
-#: All valid compoundable names.
-COMPOUNDABLES = (COLORS | _compoundables)
+#: Attributes and colors which may be compounded by underscore (``u'_'``).
+COMPOUNDABLES = _make_compoundables(COLORS)
 
 
 class ParameterizingString(six.text_type):
-    """A Unicode string which can be called as a parameterizing termcap.
+
+    r"""
+    A Unicode string which can be called as a parameterizing termcap.
 
     For example::
 
-        >> term = Terminal()
-        >> color = ParameterizingString(term.color, term.normal, 'color')
-        >> color(9)('color #9')
+        >>> term = Terminal()
+        >>> color = ParameterizingString(term.color, term.normal, 'color')
+        >>> color(9)('color #9')
         u'\x1b[91mcolor #9\x1b(B\x1b[m'
     """
 
     def __new__(cls, *args):
-        """P.__new__(cls, cap, [normal, [name]])
+        """
+        Class constructor accepting 3 positional arguments.
 
         :arg cap: parameterized string suitable for curses.tparm()
-        :arg normal: terminating sequence for this capability.
-        :arg name: name of this terminal capability.
+        :arg normal: terminating sequence for this capability (optional).
+        :arg name: name of this terminal capability (optional).
         """
         assert len(args) and len(args) < 4, args
         new = six.text_type.__new__(cls, args[0])
@@ -46,11 +66,14 @@ class ParameterizingString(six.text_type):
         return new
 
     def __call__(self, *args):
-        """P(*args) -> FormattingString()
+        """
+        Returning :class:`FormattingString` instance for given parameters.
 
         Return evaluated terminal capability (self), receiving arguments
         ``*args``, followed by the terminating sequence (self.normal) into
         a FormattingString capable of being called.
+
+        :rtype: :class:`FormattingString` or :class:`NullCallableString`
         """
         try:
             # Re-encode the cap, because tparm() takes a bytestring in Python
@@ -80,32 +103,41 @@ class ParameterizingString(six.text_type):
 
 
 class ParameterizingProxyString(six.text_type):
-    """A Unicode string which can be called to proxy missing termcap entries.
 
-    For example::
+    r"""
+    A Unicode string which can be called to proxy missing termcap entries.
 
-        >>> from blessings import Terminal
-        >>> term = Terminal('screen')
-        >>> hpa = ParameterizingString(term.hpa, term.normal, 'hpa')
-        >>> hpa(9)
-        u''
-        >>> fmt = u'\x1b[{0}G'
-        >>> fmt_arg = lambda *arg: (arg[0] + 1,)
-        >>> hpa = ParameterizingProxyString((fmt, fmt_arg), term.normal, 'hpa')
-        >>> hpa(9)
-        u'\x1b[10G'
+    This class supports the function :func:`get_proxy_string`, and mirrors
+    the behavior of :class:`ParameterizingString`, except that instead of
+    a capability name, receives a format string, and callable to filter the
+    given positional ``*args`` of :meth:`ParameterizingProxyString.__call__`
+    into a terminal sequence.
+
+    For example:
+
+    >>> from blessings import Terminal
+    >>> term = Terminal('screen')
+    >>> hpa = ParameterizingString(term.hpa, term.normal, 'hpa')
+    >>> hpa(9)
+    u''
+    >>> fmt = u'\x1b[{0}G'
+    >>> fmt_arg = lambda *arg: (arg[0] + 1,)
+    >>> hpa = ParameterizingProxyString((fmt, fmt_arg), term.normal, 'hpa')
+    >>> hpa(9)
+    u'\x1b[10G'
     """
 
     def __new__(cls, *args):
-        """P.__new__(cls, (fmt, callable), [normal, [name]])
+        """
+        Class constructor accepting 4 positional arguments.
 
         :arg fmt: format string suitable for displaying terminal sequences.
         :arg callable: receives __call__ arguments for formatting fmt.
-        :arg normal: terminating sequence for this capability.
-        :arg name: name of this terminal capability.
+        :arg normal: terminating sequence for this capability (optional).
+        :arg name: name of this terminal capability (optional).
         """
         assert len(args) and len(args) < 4, args
-        assert type(args[0]) is tuple, args[0]
+        assert isinstance(args[0], tuple), args[0]
         assert callable(args[0][1]), args[0][1]
         new = six.text_type.__new__(cls, args[0][0])
         new._fmt_args = args[0][1]
@@ -114,23 +146,33 @@ class ParameterizingProxyString(six.text_type):
         return new
 
     def __call__(self, *args):
-        """P(*args) -> FormattingString()
+        """
+        Returning :class:`FormattingString` instance for given parameters.
 
-        Return evaluated terminal capability format, (self), using callable
-        ``self._fmt_args`` receiving arguments ``*args``, followed by the
-        terminating sequence (self.normal) into a FormattingString capable
-        of being called.
+        Arguments are determined by the capability.  For example, ``hpa``
+        (move_x) receives only a single integer, whereas ``cup`` (move)
+        receives two integers.  See documentation in terminfo(5) for the
+        given capability.
+
+        :returns: class instance of :class:`FormattingString`.
+        :rtype: FormattingString
         """
         return FormattingString(self.format(*self._fmt_args(*args)),
                                 self._normal)
 
 
 def get_proxy_string(term, attr):
-    """ Proxy and return callable StringClass for proxied attributes.
+    """
+    Proxy and return callable string for proxied attributes.
 
-    We know that some kinds of terminal kinds support sequences that the
-    terminfo database always report -- such as the 'move_x' attribute for
-    terminal type 'screen' and 'ansi', or 'hide_cursor' for 'ansi'.
+    :param blessings.Terminal term: :class:`~.Terminal` instance.
+    :param str attr: terminal capability name that may be proxied.
+    :rtype: None or :class:`ParameterizingProxyString`.
+    :returns: :class:`ParameterizingProxyString` for some attributes
+        of some terminal types that support it, where the terminfo(5)
+        database would otherwise come up empty, such as ``move_x``
+        attribute for ``term.kind`` of ``screen``.  Otherwise, None.
+
 
     Returns instance of ParameterizingProxyString or NullCallableString.
     """
@@ -160,18 +202,29 @@ def get_proxy_string(term, attr):
 
 
 class FormattingString(six.text_type):
-    """A Unicode string which can be called using ``text``,
-    returning a new string, ``attr`` + ``text`` + ``normal``::
 
-        >> style = FormattingString(term.bright_blue, term.normal)
-        >> style('Big Blue')
-        u'\x1b[94mBig Blue\x1b(B\x1b[m'
+    r"""
+    A Unicode string which doubles as a callable.
+
+    This is used for terminal attributes, so that it may be used both
+    directly, or as a callable.  When used directly, it simply emits
+    the given terminal sequence.  When used as a callable, it wraps the
+    given (string) argument with the 2nd argument used by the class
+    constructor.
+
+    >>> style = FormattingString(term.bright_blue, term.normal)
+    >>> print(repr(style))
+    u'\x1b[94m'
+    >>> style('Big Blue')
+    u'\x1b[94mBig Blue\x1b(B\x1b[m'
     """
 
     def __new__(cls, *args):
-        """P.__new__(cls, sequence, [normal])
+        """
+        Class constructor accepting 2 positional arguments.
+
         :arg sequence: terminal attribute sequence.
-        :arg normal: terminating sequence for this attribute.
+        :arg normal: terminating sequence for this attribute (optional).
         """
         assert 1 <= len(args) <= 2, args
         new = six.text_type.__new__(cls, args[0])
@@ -179,27 +232,29 @@ class FormattingString(six.text_type):
         return new
 
     def __call__(self, text):
-        """P(text) -> unicode
-
-        Return string ``text``, joined by specified video attribute,
-        (self), and followed by reset attribute sequence (term.normal).
-        """
+        """Return ``text`` joined by ``sequence`` and ``normal``."""
         if len(self):
             return u''.join((self, text, self._normal))
         return text
 
 
 class NullCallableString(six.text_type):
-    """A dummy callable Unicode to stand in for ``FormattingString`` and
-    ``ParameterizingString`` for terminals that cannot perform styling.
+
     """
+    A dummy callable Unicode alternative to :class:`FormattingString`.
+
+    This is used for colors on terminals that do not support colors,
+    it is just a basic form of unicode that may also act as a callable.
+    """
+
     def __new__(cls):
+        """Class constructor."""
         new = six.text_type.__new__(cls, u'')
         return new
 
     def __call__(self, *args):
-        """Return a Unicode or whatever you passed in as the first arg
-        (hopefully a string of some kind).
+        """
+        Allow empty string to be callable, returning given string, if any.
 
         When called with an int as the first arg, return an empty Unicode. An
         int is a good hint that I am a ``ParameterizingString``, as there are
@@ -235,27 +290,38 @@ class NullCallableString(six.text_type):
 
 
 def split_compound(compound):
-    """Split a possibly compound format string into segments.
+    """
+    Split compound formating string into segments.
 
     >>> split_compound('bold_underline_bright_blue_on_red')
     ['bold', 'underline', 'bright_blue', 'on_red']
 
+    :param str compound: a string that may contain compounds,
+       separated by underline (``_``).
+    :returns: list of at least 1 length containing strings.
+    :rtype: list
     """
     merged_segs = []
     # These occur only as prefixes, so they can always be merged:
     mergeable_prefixes = ['on', 'bright', 'on_bright']
-    for s in compound.split('_'):
+    for segment in compound.split('_'):
         if merged_segs and merged_segs[-1] in mergeable_prefixes:
-            merged_segs[-1] += '_' + s
+            merged_segs[-1] += '_' + segment
         else:
-            merged_segs.append(s)
+            merged_segs.append(segment)
     return merged_segs
 
 
 def resolve_capability(term, attr):
-    """Return a Unicode string for the terminal capability ``attr``,
-    or an empty string if not found, or if terminal is without styling
-    capabilities.
+    """
+    Resolve a raw terminal capability using :func:`tigetstr`.
+
+    :param blessings.Terminal term: :class:`~.Terminal` instance.
+    :param str attr: terminal capability name (see terminfo(5)).
+    :returns: string of the given terminal capability named by ``attr``,
+       which may be empty (``u''``) if not found or not supported by the
+       given ``term.kind``.
+    :rtype: unicode
     """
     # Decode sequences as latin1, as they are always 8-bit bytes, so when
     # b'\xff' is returned, this must be decoded to u'\xff'.
@@ -266,14 +332,22 @@ def resolve_capability(term, attr):
 
 
 def resolve_color(term, color):
-    """resolve_color(T, color) -> FormattingString()
-
-    Resolve a ``color`` name to callable capability, ``FormattingString``
-    unless ``term.number_of_colors`` is 0, then ``NullCallableString``.
-
-    Valid ``color`` capabilities names are any of the simple color
-    names, such as ``red``, or compounded, such as ``on_bright_green``.
     """
+    Resolve a simple color name to a callable capability.
+
+    This function supports :func:`resolve_attribute`.
+
+    :param blessings.Terminal term: :class:`~.Terminal` instance.
+    :param str color: any string found in set :var:`COLORS`.
+    :returns: a string class instance which emits the terminal sequence
+        for the given color, and may be used as a callable to wrap the
+        given string with such sequence.
+    :rtype: :class:`NullCallableString` when ``term.number_of_colors`` is 0,
+        otherwise :class:`FormattingString`.
+    """
+    if term.number_of_colors == 0:
+        return NullCallableString()
+
     # NOTE(erikrose): Does curses automatically exchange red and blue and cyan
     # and yellow when a terminal supports setf/setb rather than setaf/setab?
     # I'll be blasted if I can find any documentation. The following
@@ -285,8 +359,6 @@ def resolve_color(term, color):
     # bright colors at 8-15:
     offset = 8 if 'bright_' in color else 0
     base_color = color.rsplit('_', 1)[-1]
-    if term.number_of_colors == 0:
-        return NullCallableString()
 
     attr = 'COLOR_%s' % (base_color.upper(),)
     fmt_attr = color_cap(getattr(curses, attr) + offset)
@@ -294,11 +366,19 @@ def resolve_color(term, color):
 
 
 def resolve_attribute(term, attr):
-    """Resolve a sugary or plain capability name, color, or compound
-    formatting name into a *callable* unicode string capability,
-    ``ParameterizingString`` or ``FormattingString``.
     """
-    # A simple color, such as `red' or `blue'.
+    Resolve a terminal attribute name into a capability class.
+
+    :param blessings.Terminal term: :class:`~.Terminal` instance.
+    :param str attr: Sugary, ordinary, or compound formatted terminal
+        capability, such as "red_on_white", "normal", "red", or
+        "bold_on_black", respectively.
+    :returns: a string class instance which emits the terminal sequence
+        for the given terminal capability, or may be used as a callable to
+        wrap the given string with such sequence.
+    :rtype: :class:`NullCallableString` if the given ``term.number_of_colors``
+        is 0, otherwise :class:`FormattingString`.
+    """
     if attr in COLORS:
         return resolve_color(term, attr)
 
