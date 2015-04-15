244e24ff414312403f49eba1ef92d1b308e2c092
Jeff Quast <jquast@io.com>
2015-04-15 00:32:57 -0700

    Fix all bin/*.py and add docs/examples.rst

--- bin/tprint.py
+++ bin/tprint.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
-
+"""A simple cmd-line tool for displaying FormattingString capabilities."""
+from __future__ import print_function
 import argparse
 
-from blessings import Terminal
+def main():
+    """Program entry point."""
+    from blessings import Terminal
+
+    parser = argparse.ArgumentParser(
+        description='displays argument as specified style')
+
+    parser.add_argument('style', type=str, help='style formatter')
+    parser.add_argument('text', type=str, nargs='+')
 
 
-parser = argparse.ArgumentParser(
-    description='displays argument as specified style')
+    term = Terminal()
+    args = parser.parse_args()
 
-parser.add_argument('style', type=str, help='style formatter')
-parser.add_argument('text', type=str, nargs='+')
+    style = getattr(term, args.style)
 
+    print(style(' '.join(args.text)))
 
-term = Terminal()
-args = parser.parse_args()
-
-style = getattr(term, args.style)
-
-print(style(' '.join(args.text)))
+if __name__ == '__main__':
+    main()
--- docs/index.rst
+++ docs/index.rst
@@ -10,6 +10,7 @@ Contents:
 
    intro
    overview
+   examples
    further
    pains
    api
